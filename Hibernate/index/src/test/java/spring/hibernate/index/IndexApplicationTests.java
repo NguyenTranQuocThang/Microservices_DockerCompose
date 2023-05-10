b3a1245274094c9743c57e09a4ed00c93363cd1b
= <=>
2023-05-10 20:59:46 +0700

    create repo

---
+++ Hibernate/index/src/test/java/spring/hibernate/index/IndexApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.hibernate.index;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
